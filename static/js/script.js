74c76d40c45a6096288be4df3aacaff52bd5ae50
franco057 <89106956+franco057@users.noreply.github.com>
2024-05-20 13:28:54 +0000

    modified:   index.html 	new file:   source/img/F18/A10.jpeg 	new file:   source/img/F18/AH-64D.jpg 	new file:   source/img/F18/EF2000.jpeg 	new file:   source/img/F18/F15.jpeg 	new file:   source/img/F18/F16blk70.jpeg 	new file:   source/img/F18/F22.jpeg

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,9 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+document.addEventListener('DOMContentLoaded', function() {
+  var buttons = document.querySelectorAll('#btn');
+  buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+          var url = this.getAttribute('data-url');
+          window.location.href = url;
+      });
+  });
 });
